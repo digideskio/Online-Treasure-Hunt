3ece43cf22ac1007cb33861838709bbb073bbdcb
Ritesh Panjwani <riteshpanjwani@users.noreply.github.com>
2015-05-07 13:48:59 +0530

    Update database_connection.php

--- app/login/database_connection.php
+++ app/login/database_connection.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ERROR);
-define ('hostnameorservername','localhost'); //Your server name or hostname goes in here
-define ('serverusername','riteshp'); //Your database username goes in here
-define ('serverpassword','adsftr$67');  //Your database password goes in here
-define ('databasenamed','oth2014');  //Your database name goes in here
+define ('hostnameorservername',''); //Your server name or hostname goes in here
+define ('serverusername',''); //Your database username goes in here
+define ('serverpassword','');  //Your database password goes in here
+define ('databasenamed','');  //Your database name goes in here
 
 global $connection;
 $con = mysqli_connect(hostnameorservername,serverusername,serverpassword,databasenamed) or die(mysqli_error($con));
